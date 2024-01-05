89e4a1d4340f048227a47b97300346580d9d65b2
Usman Shahid <140746601+UsmanShahid05@users.noreply.github.com>
2024-01-05 11:16:48 +0500

    Use of comments in python
    Comments are used as description of any code or lines of code for better understanding, future understanding of code, ease for use of other

---
+++ comments.py
@@ -0,0 +1,19 @@
+'''
+Comments:- Comments are description of any code or lines of code for future understanding or for ease of other use of use.
+'''
+
+# In python there are two types of comments 
+# Type one: Single_line_comment: This comment is used for just single line
+# Syntex of Single_line_comment is" #" before any comment we place " # ", and then write our comment.
+
+# This is single line comment.
+# We can use comment beside any code also , like following
+var_with_comment="Hello World" # This line will execute but we can place comment near to line for description of the code for future and better understanding.
+
+# Type two: Multiple_line_Comment: This comment is used for multiple line which we don't want to be executed while executing whole code.
+# Syntex of multiple_line_Comment is by using triple qutation i.e ''', at start and ''' at end
+'''
+Any thing written in between these quotes will be consider as comment and will not run during programe execution.
+we use this comment for multiple lines 
+'''
+print("value")
